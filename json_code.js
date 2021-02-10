be25acf3b37a3bee8c1471be9f75139555a955da
gov <gabcomp.gov@gmail.com>
2021-02-10 00:39:20 -0300

    v1.0.2
    docs new json structure

--- json_code.js
+++ json_code.js
@@ -1,2 +1,209 @@
+var json_= [["StartPage"],[[3,3,2],["卂丂爪","Oƒƒice","ĐØ₵𝓼...","ⓉⒺⓧⓉ","S🌎cial","ℝ𝕒𝕟𝕕𝕠𝕞","","About Me"],[4,4,4,4,4,4,8,4],[21,21,21,21,21,21,12,23]],[[["Nasm Doc","https://www.nasm.us/doc/nasmdocb.html"],["MIPS ASM","https://chortle.ccsu.edu/AssemblyTutorial/index.html"],["X86 Opcode","http://ref.x86asm.net/coder64.html"],["Syscall w3chall","https://syscalls.w3challs.com/"],["ASM-Basic","https://www.tutorialspoint.com/assembly_programming/"]],[["Gmail","https://www.google.com/intl/en/gmail/about/"],["Notion","https://www.notion.so/"],["Dillinger","https://dillinger.io/"],["Draw.IO","https://app.diagrams.net/"]],[["",""],["DEV-docs","https://devdocs.io/settings"],["[W3]schools","https://www.w3schools.com/"],["CPP","http://www.cplusplus.com/reference/"],["Octave","https://octave.org/doc/v6.1.0/"],["Python","https://docs.python.org/3/tutorial/datastructures.html"]],[["Unicode List","https://www.isthisthingon.org/unicode/?page=0F&subpage=D"],["Fancy-Text","https://nickfinder.com/fancy-text"],["Ascii Art","https://ascii.co.uk/art"],["Cryptii","https://cryptii.com/"],["(J)emoticons","https://www.jemoticons.com/en"]],[["Twitter","https://twitter.com/twitter"],["Instagram","https://www.instagram.com/instagram"],["Youtube","https://www.youtube.com/"],["Facebook","https://www.facebook.com/GitHub/"],["Discord","https://discord.com/login"],["Reddit","https://www.reddit.com/"],["GitHub","https://github.com/explore"]],[["JSON Editor","https://jsoneditoronline.org/"],["FileFormat","https://www.fileformat.info/index.htm"],["RosettaCode","https://rosettacode.org/wiki/Category:Octave"],["Science General","https://www.cs.utah.edu/~germain/PPS/Topics/"],["Desmos","https://www.desmos.com/calculator"],["FlatIcon","https://www.flaticon.com/"],["Instructables","https://www.instructables.com/"]],[],[["",""],["GitHub Profile","https://github.com/GabriOliv"],["",""],["Front-End with Bootstrap 5.0","https://getbootstrap.com/docs/versions/"],["",""],["Page hosted with GitHub Pages.","https://pages.github.com/"]]]]
 
-var json_= [["StartPage"],[["卂丂爪","[C][OCT.m]","Oƒƒice","ĐØ₵𝓼...","ⓉⒺⓧⓉ","ℝ𝕒𝕟𝕕𝕠𝕞","","Repository"],[1,1,1,2,2,3,3,3],[5,12,9,4,1,15,3,3]],[[["Nasm Doc","https://www.nasm.us/doc/nasmdocb.html"],["MIPS ASM","https://chortle.ccsu.edu/AssemblyTutorial/index.html"],["X86 Opcode","http://ref.x86asm.net/coder64.html"],["Syscall w3chall","https://syscalls.w3challs.com/"],["ASM-Basic","https://www.tutorialspoint.com/assembly_programming/"]],[["C++ Ref","http://www.cplusplus.com/reference/"],["C_Subroutines","http://users.cs.cf.ac.uk/Dave.Marshall/C/"],["Octave Doc","https://octave.org/doc/v6.1.0/"],["RosettaCode","https://rosettacode.org/wiki/Category:Octave"],["Science General","https://www.cs.utah.edu/~germain/PPS/Topics/"]],[["Gmail","https://www.google.com/intl/en/gmail/about/"],["Notion","https://www.notion.so/"],["Dillinger","https://dillinger.io/"],["Draw.IO","https://app.diagrams.net/"]],[["",""],["[W3]schools","https://www.w3schools.com/"],["DEV-docs","https://devdocs.io/settings"],["Metasploit","https://docs.rapid7.com/metasploit/"]],[["Unicode List","https://www.isthisthingon.org/unicode/?page=0F&subpage=D"],["Fancy-Text","https://nickfinder.com/fancy-text"],["Ascii Art","https://ascii.co.uk/art"],["Cryptii","https://cryptii.com/"],["(J)emoticons","https://www.jemoticons.com/en"]],[["JSON Editor","https://jsoneditoronline.org/"],["FileFormat","https://www.fileformat.info/index.htm"],["Desmos","https://www.desmos.com/calculator"],["FlatIcon","https://www.flaticon.com/"],["Instructables","https://www.instructables.com/"]],[],[["",""],["Git Profile","https://github.com/GabriOliv"],["",""],["hosted with GitHub Pages.","https://pages.github.com/"]]]]
+/*
+
+[
+    [
+        "StartPage"
+    ],
+    [
+        [
+            3,
+            3,
+            2
+        ],
+        [
+            "卂丂爪",
+            "[C][OCT.m]",
+            "Oƒƒice",
+            "ĐØ₵𝓼...",
+            "ⓉⒺⓧⓉ",
+            "ℝ𝕒𝕟𝕕𝕠𝕞",
+            "",
+            "About Me"
+        ],
+        [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            8,
+            4
+        ],
+        [
+            21,
+            21,
+            21,
+            21,
+            21,
+            21,
+            12,
+            23
+        ]
+    ],
+    [
+        [
+            [
+                "Nasm Doc",
+                "https://www.nasm.us/doc/nasmdocb.html"
+            ],
+            [
+                "MIPS ASM",
+                "https://chortle.ccsu.edu/AssemblyTutorial/index.html"
+            ],
+            [
+                "X86 Opcode",
+                "http://ref.x86asm.net/coder64.html"
+            ],
+            [
+                "Syscall w3chall",
+                "https://syscalls.w3challs.com/"
+            ],
+            [
+                "ASM-Basic",
+                "https://www.tutorialspoint.com/assembly_programming/"
+            ]
+        ],
+        [
+            [
+                "C++ Ref",
+                "http://www.cplusplus.com/reference/"
+            ],
+            [
+                "C_Subroutines",
+                "http://users.cs.cf.ac.uk/Dave.Marshall/C/"
+            ],
+            [
+                "Octave Doc",
+                "https://octave.org/doc/v6.1.0/"
+            ],
+            [
+                "RosettaCode",
+                "https://rosettacode.org/wiki/Category:Octave"
+            ],
+            [
+                "Science General",
+                "https://www.cs.utah.edu/~germain/PPS/Topics/"
+            ]
+        ],
+        [
+            [
+                "Gmail",
+                "https://www.google.com/intl/en/gmail/about/"
+            ],
+            [
+                "Notion",
+                "https://www.notion.so/"
+            ],
+            [
+                "Dillinger",
+                "https://dillinger.io/"
+            ],
+            [
+                "Draw.IO",
+                "https://app.diagrams.net/"
+            ]
+        ],
+        [
+            [
+                "",
+                ""
+            ],
+            [
+                "[W3]schools",
+                "https://www.w3schools.com/"
+            ],
+            [
+                "DEV-docs",
+                "https://devdocs.io/settings"
+            ],
+            [
+                "Python",
+                "https://docs.python.org/3/tutorial/datastructures.html"
+            ],
+            [
+                "Rust Regex(to VSC)",
+                "https://docs.rs/regex/1.4.3/regex/"
+            ],
+            [
+                "Metasploit",
+                "https://docs.rapid7.com/metasploit/"
+            ]
+        ],
+        [
+            [
+                "Unicode List",
+                "https://www.isthisthingon.org/unicode/?page=0F&subpage=D"
+            ],
+            [
+                "Fancy-Text",
+                "https://nickfinder.com/fancy-text"
+            ],
+            [
+                "Ascii Art",
+                "https://ascii.co.uk/art"
+            ],
+            [
+                "Cryptii",
+                "https://cryptii.com/"
+            ],
+            [
+                "(J)emoticons",
+                "https://www.jemoticons.com/en"
+            ]
+        ],
+        [
+            [
+                "JSON Editor",
+                "https://jsoneditoronline.org/"
+            ],
+            [
+                "FileFormat",
+                "https://www.fileformat.info/index.htm"
+            ],
+            [
+                "Desmos",
+                "https://www.desmos.com/calculator"
+            ],
+            [
+                "FlatIcon",
+                "https://www.flaticon.com/"
+            ],
+            [
+                "Instructables",
+                "https://www.instructables.com/"
+            ]
+        ],
+        [],
+        [
+            [
+                "",
+                ""
+            ],
+            [
+                "GitHub Profile",
+                "https://github.com/GabriOliv"
+            ],
+            [
+                "",
+                ""
+            ],
+            [
+                "Front-End with Bootstrap 5.0",
+                "https://getbootstrap.com/docs/versions/"
+            ],
+            [
+                "",
+                ""
+            ],
+            [
+                "Page hosted with GitHub Pages.",
+                "https://pages.github.com/"
+            ]
+        ]
+    ]
+]
+
+*/
